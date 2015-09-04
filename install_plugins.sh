18792434a4e5fee17d994a4fbedefca775461608
Gerhard Gappmeier <gerhard.gappmeier@ascolab.com>
2015-09-04 18:46:53 +0200

    Add VimDir plugin

--- install_plugins.sh
+++ install_plugins.sh
@@ -13,6 +13,8 @@ PLUGINS="$PLUGINS git://github.com/gergap/vim-kanban.git"
 PLUGINS="$PLUGINS https://github.com/airblade/vim-gitgutter.git"
 # indentation Lines
 PLUGINS="$PLUGINS https://github.com/Yggdroot/indentLine.git"
+# dirdiff plugin
+PLUGINS="$PLUGINS https://github.com/will133/vim-dirdiff.git"
 # StartTimPope: Tim Pope is a Vim Hero: let's use some of his awsome plugins
 # vim-fugitive - Awesome git plugin
 PLUGINS="$PLUGINS git://github.com/tpope/vim-fugitive.git"
